8a0503f6bb7f50de21241f8e4d15c83fbe41c86f
first-wanderer <kaa-master37@mail.ru>
2015-05-31 12:19:28 +0400

    AJAX for form

--- js/script.js
+++ js/script.js
@@ -55,3 +55,38 @@ google.maps.event.addDomListener(window, "load", initialize);
   };
 
 })();
+
+//AJAX отправка формы
+(function(){
+  if (!("FormData" in window)) {
+    return;
+  };
+
+  var form = document.querySelector("#form-contest");
+  
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    var data = new FormData(form);
+
+    request(data, function(response) {
+      console.log(response);
+    });
+  });
+
+  function request(data, fn) {
+    var xhr = new XMLHttpRequest();
+    var time = (new Date()).getTime();
+
+    xhr.open("post", "http://simonenko.su/academy/echo?" + time);
+
+    xhr.addEventListener("readystatechange", function() {
+      if (xhr.readyState == 4) {
+        fn(xhr.responseText);
+      };
+    });
+
+    xhr.send(data);
+  };
+
+})();
