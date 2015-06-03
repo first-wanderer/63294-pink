08df7936293107293a7105e7ef9413ebd3b6e513
first-wanderer <kaa-master37@mail.ru>
2015-06-03 08:13:49 +0400

    Removal of comments 03.06.15

--- js/script.js
+++ js/script.js
@@ -1,92 +1,100 @@
-//добавление интерактивной карты
-function initialize() {  
-  var centerLatlng = new google.maps.LatLng(59.938910, 30.323031);
-  var mapOptions = {
-    zoom: 17,
-    center: centerLatlng
-  }
-  var map = new google.maps.Map(document.getElementById("google-map"), mapOptions);
-  
-  var image = 'img/map-marker.svg';
-  var myLatlng = new google.maps.LatLng(59.938641, 30.323010);
-  var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,      
-      icon: image
-  });
-}
+(function() {
+'use strict';
 
-google.maps.event.addDomListener(window, "load", initialize);
-
-
-//Изменение значений числовых полей
-(function(){
-  var elements = document.querySelectorAll(".input-range");
-
-  for (var i = 0; i < elements.length; i++) {
-    initRange(elements[i]);
-  };
-
-  function initRange(parent) {
-    var input = parent.querySelector("input");
-    var minus = parent.querySelector(".range-minus");
-    var plus = parent.querySelector(".range-plus");
-
-    minus.addEventListener("click", function() {
-      changeRange(false);
-    });
-    plus.addEventListener("click", function() {
-      changeRange(true);
-    });
-
-    function changeRange(operation) {
-      var value = Number(input.value);
+  //добавление интерактивной карты
+  (function(){
+    function initialize() {  
+      var centerLatlng = new google.maps.LatLng(59.938910, 30.323031);
+      var mapOptions = {
+        zoom: 17,
+        center: centerLatlng
+      };
+      var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
       
-      if (isNaN(value) || value < 1) {
-        value = 1;
-      };
+      var image = 'img/map-marker.svg';
+      var myLatlng = new google.maps.LatLng(59.938641, 30.323010);
+      new google.maps.Marker({
+          position: myLatlng,
+          map: map,      
+          icon: image
+      });
+    }
 
-      if (operation) {
-        input.value = value + 1;
-      } else {
-        input.value = value - 1;
-      };
-    };
-  };
+    if(document.querySelector('#google-map')) {
+      google.maps.event.addDomListener(window, 'load', initialize);
+    }  
+  })();
 
-})();
+  //Изменение значений числовых полей
+  (function(){
+    var elements = document.querySelectorAll('.input-range');
 
-//AJAX отправка формы
-(function(){
-  if (!("FormData" in window)) {
-    return;
-  };
+    function initRange(parent) {
+      var input = parent.querySelector('input');
+      var minus = parent.querySelector('.range-minus');
+      var plus = parent.querySelector('.range-plus');
 
-  var form = document.querySelector("#form-contest");
-  
-  form.addEventListener("submit", function(event) {
-    event.preventDefault();
+      minus.addEventListener('click', function() {
+        changeRange(false);
+      });
+      plus.addEventListener('click', function() {
+        changeRange(true);
+      });
 
-    var data = new FormData(form);
+      function changeRange(operation) {
+        var value = Number(input.value);
+        
+        if (isNaN(value) || value < 1) {
+          value = 1;
+        }
 
-    request(data, function(response) {
-      console.log(response);
-    });
-  });
+        if (operation) {
+          input.value = value + 1;
+        } else {
+          input.value = value - 1;
+        }
+      }
+    }
 
-  function request(data, fn) {
-    var xhr = new XMLHttpRequest();
-    var time = (new Date()).getTime();
+    for (var i = 0; i < elements.length; i++) {
+      initRange(elements[i]);
+    }    
+  })();
 
-    xhr.open("post", "http://simonenko.su/academy/echo?" + time);
+  //AJAX отправка формы
+  (function(){
+    if (!('FormData' in window)) {
+      return;
+    }
 
-    xhr.addEventListener("readystatechange", function() {
-      if (xhr.readyState == 4) {
-        fn(xhr.responseText);
-      };
-    });
+    var form = document.querySelector('#form-contest');
 
-    xhr.send(data);
-  };
+    function request(data, fn) {
+      var xhr = new XMLHttpRequest();
+      var time = (new Date()).getTime();
 
-})();
+      xhr.open('post', 'http://simonenko.su/academy/echo?' + time);
+
+      xhr.addEventListener('readystatechange', function() {
+        if (xhr.readyState == 4) {
+          fn(xhr.responseText);
+        }
+      });
+
+      xhr.send(data);
+    }
+    
+    if (form) {
+      form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        var data = new FormData(form);
+
+        request(data, function(response) {
+          console.log(response);
+        });
+      });
+    }
+  })();
+
+}());
